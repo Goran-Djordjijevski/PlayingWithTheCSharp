9703d0f21e5a218e3955c17643355284cc99fa08
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-09-27 20:42:25 +0200

    Added new project Grade.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        IList<float> grades;
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+    }
+}
