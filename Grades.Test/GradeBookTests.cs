87c2a3ba93b372ff880465ee811893c0ee27947a
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-01 10:25:54 +0200

    Added new UnitTest project.

---
+++ Grades.Test/GradeBookTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Test
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            //-- Arrange
+            // Create instance of the class
+
+            // Create expected value
+
+            //-- Act
+            // Create actual value from object instance
+
+            //-- Assert
+            // Test
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
