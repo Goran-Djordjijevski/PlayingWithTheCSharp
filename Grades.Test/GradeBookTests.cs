386219157af3b1d80875dbc35c644f9c4ed00f66
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-01 10:41:26 +0200

    Added new methods to the UnitTest.

--- Grades.Test/GradeBookTests.cs
+++ Grades.Test/GradeBookTests.cs
@@ -21,5 +21,38 @@ namespace Grades.Test
             //-- Assert
             Assert.AreEqual(90, result.HighestGrade);
         }
+
+        [TestMethod]
+        public void ComputesLowestGrade()
+        {
+            //-- Arrange
+            GradeBook book = new GradeBook();
+
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            //-- Act
+            GradeStatistics result = book.ComputeStatistics();
+
+            //-- Assert
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesAverageGrade()
+        {
+            //-- Arrange
+            GradeBook book = new GradeBook();
+
+            book.AddGrade(grade: 91F);
+            book.AddGrade(grade: 89.5F);
+            book.AddGrade(grade: 75F);
+
+            //-- Act
+            GradeStatistics result = book.ComputeStatistics();
+
+            //-- Assert
+            Assert.AreEqual(85.16, result.AverageGrade, 0.01);
+        }
     }
 }
