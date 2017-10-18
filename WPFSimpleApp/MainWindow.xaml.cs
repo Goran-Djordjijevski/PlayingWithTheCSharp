8dab936d03f9522d51b756f65e57f8e6916cedeb
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-18 12:33:27 +0200

    Added new WPF Project.

---
+++ WPFSimpleApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSimpleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Employee e1 = new Employee();
+            e1.Name = "Goran";
+
+            Employee e2 = new Employee();
+            e2.Name = "Djordjijevski";
+
+            Output.Text = $"{e1.Name} {e2.Name}";
+        }
+    }
+}
