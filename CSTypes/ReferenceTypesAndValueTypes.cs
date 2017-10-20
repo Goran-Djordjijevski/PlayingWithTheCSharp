e01536f0a57ea067c099ece595896d2844c8a292
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-20 08:33:48 +0200

    Added new test folder and new test class.

---
+++ CSTypes/ReferenceTypesAndValueTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSTypes
+{
+    public class Invoice
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    [TestClass]
+    public class ReferenceTypesAndValueTypes
+    {
+        [TestMethod]
+        public void IdentityTest()
+        {
+            Invoice firstInvoice = new Invoice
+            {
+                ID = 1,
+                Description = "Test",
+                Amount = 0.0M
+            };
+
+            Invoice secondInvoice = new Invoice
+            {
+                ID = 1,
+                Description = "Test",
+                Amount = 0.0M
+            };
+
+
+        }
+    }
+}
